7f1d95fb64c8c70c33603e9892081e55f255d555
Bowen Zheng <bzheng2438@gmail.com>
2026-08-23 22:27:52 -0400

    Updates

---
+++ static/js/534.e83db29a.chunk.js
@@ -0,0 +1,81 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[534],{534:(e,t,i)=>{i.r(t),i.d(t,{default:()=>f});var n=i(5043);const o=JSON.parse('{"DD":"2-AXIS NANOPOSITIONER","Qq":"Low-cost open-source instrument for nanoscale manipulation","x6":[{"title":"Build your own","link":"https://github.com/bwnzhng/Open-Nano"},{"title":"Learn more","color":"#fff"}]}'),r=JSON.parse('{"DD":"Everything you need","Qq":"","uW":[{"title":"Nanopositioner","content":"3D print or machine your own with the included CAD files, engineering drawings, and build instructions","icon":"nanopositioner.svg"},{"title":"Nano-Core","content":"Assemble your own controller with the included firmware code, electrical schematics, and wiring diagrams","icon":"controller.svg"}]}'),a=JSON.parse('{"D":"Product Specifications","Q":"Testing was conducted under a range of operating conditions to evaluate the nanopositioner. Following the ISO 230-2 procedures for assessing positioning performance, the current specs are summarized below: "}'),s="",d="",l=JSON.parse('{"D":"Contact Form","Q":"Any questions? Reach out if you can\'t find what you\'re looking for."}');var c=i(2026),m=i(579);const p=[{headers:["Overview","Mechanical"],rows:[["Travel [mm]","25"],["Dimensions [mm]","26 x 55 x 55"],["Load Capacity [g]","300"],["Open-Loop Speed [mm/s]","1.25"],["Resolution [nm]","50"]]},{headers:["X-Stage","Closed-Loop"],rows:[["Max Speed [\xb5m/s]","144"],["Positioning Error [nm]","305.2"],["Bi-Directional Repeatability [\xb5m]","1.087"]]},{headers:["Y-Stage","Closed-Loop"],rows:[["Max Speed [\xb5m/s]","195"],["Positioning Error [nm]","546.1"],["Bi-Directional Repeatability [\xb5m]","0.776"]]}],h=(0,n.lazy)((()=>Promise.all([i.e(91),i.e(545),i.e(676)]).then(i.bind(i,1676)))),g=(0,n.lazy)((()=>Promise.resolve().then(i.bind(i,1766)))),x=(0,n.lazy)((()=>i.e(538).then(i.bind(i,8538)))),u=(0,n.lazy)((()=>Promise.all([i.e(91),i.e(282)]).then(i.bind(i,3282)))),f=()=>(0,m.jsxs)(g,{children:[(0,m.jsx)(x,{}),(0,m.jsx)(u,{direction:"right",title:o.DD,content:o.Qq,button:o.x6,icon:"landing_image.svg",id:"intro"}),(0,m.jsx)(u,{direction:"left",title:s,content:d,id:"product"}),(0,m.jsx)(u,{direction:"left",title:r.DD,content:r.Qq,section:r.uW,icon:"cover.svg",id:"about"}),(0,m.jsx)(u,{direction:"right",title:a.D,content:a.Q,id:"mission",belowIcon:(0,m.jsx)(m.Fragment,{children:p.map((e=>{let{headers:t,rows:i}=e;return(0,m.jsxs)(c.iV,{children:[(0,m.jsx)("thead",{children:(0,m.jsxs)("tr",{children:[(0,m.jsx)("th",{children:t[0]}),(0,m.jsx)("th",{children:t[1]})]})}),(0,m.jsx)("tbody",{children:i.map((e=>{let[t,i]=e;return(0,m.jsxs)("tr",{children:[(0,m.jsx)("td",{children:t}),(0,m.jsx)("td",{children:i})]},t)}))})]},t[0])}))})}),(0,m.jsx)(h,{title:l.D,content:l.Q,id:"contact"})]})},2026:(e,t,i)=>{i.d(t,{Fe:()=>r,HD:()=>d,UC:()=>a,dG:()=>m,eM:()=>p,iV:()=>h,j_:()=>c,xv:()=>l,yX:()=>s});var n=i(1319),o=i(9701);const r=(0,o.Ay)("section")`
+  position: relative;
+  padding: 3rem 0 5rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,a=(0,o.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,s=(0,o.Ay)(n.A)`
+  flex-direction: ${e=>{let{direction:t}=e;return"left"===t?"row":"row-reverse"}};
+`,d=(0,o.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,l=(0,o.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,c=(0,o.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,m=(0,o.Ay)("p")`
+  font-size: 13px;
+`,p=(0,o.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,h=(0,o.Ay)("table")`
+  width: 100%;
+  margin-top: 2rem;
+  border-collapse: collapse;
+  font-size: 0.8rem;
+  color: #434343;
+
+  th,
+  td {
+    border: 1px solid #dfe5e8;
+    padding: 0.6rem 0.45rem;
+    text-align: left;
+  }
+
+  th {
+    background: #2e186a;
+    color: #fff;
+    font-weight: 700;
+  }
+
+  tr:nth-child(even) td {
+    background: #f6f8f9;
+  }
+
+  td:last-child {
+    text-align: right;
+  }
+
+  @media only screen and (max-width: 575px) {
+    font-size: 0.7rem;
+
+    th,
+    td {
+      padding: 0.45rem 0.3rem;
+    }
+  }
+`}}]);
+//# sourceMappingURL=534.e83db29a.chunk.js.map
