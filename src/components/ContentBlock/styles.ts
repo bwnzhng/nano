a015351c03302b012dcc6a581a751a567326432c
Bowen Zheng <bzheng2438@gmail.com>
2026-08-23 22:37:58 -0400

    Update styles.ts

--- src/components/ContentBlock/styles.ts
+++ src/components/ContentBlock/styles.ts
@@ -6,7 +6,7 @@ export const ContentSection = styled("section")`
   padding: 3rem 0 5rem;
 
   @media only screen and (max-width: 1024px) {
-    padding: 4rem 0 4rem;
+    padding: 2rem 0 2rem;
   }
 `;
 
